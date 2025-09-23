1f0bfccc91c80d08e8b731d571e1e7133ec23dce
lauramarin3131 <lauramarin3131@gmail.com>
2025-09-23 12:49:09 +0100

    Add task: form added

--- tasky/src/App.jsx
+++ tasky/src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css';
 import Task from './components/Task';
 import React, { useState } from 'react';
+import AddTaskForm from './components/Form';
 
 
 function App() {
@@ -40,7 +41,7 @@ function App() {
 
           />
         ))} 
-
+        <AddTaskForm />
     </div>
   );
 }
